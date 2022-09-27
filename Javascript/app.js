663ec6dcca74125dbaf66aa7e3f66afdf4fa2a2c
dun <DundeeDevelops@gmail.com>
2022-09-27 17:16:07 -0500

    Added location marker

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,7 +1,15 @@
-let map = L.map('map').setView([41.79508283290762, -87.6781578791031],16);
+let cords = [42.84897787076336, -106.29737773388705];
+let map = L.map('map');
+updateView();
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 20
 }).addTo(map); 
+
+// Create marker and focus view on it
+function updateView(){
+   L.marker(cords).addTo(map);
+   map.setView(cords,18);
+}
