ca8cf4575d281cb4d6fab9b35f9c4b2166ae752c
dun <DundeeDevelops@gmail.com>
2022-09-26 23:20:57 -0500

    Added in the maps API

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([56, 106],10);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
