ca4e8855bb4a1d4f8c35f241d7bec2dd38fbcc3f
dun <DundeeDevelops@gmail.com>
2022-09-27 21:49:58 -0500

    Form validation
    Added form validation to make sure the user has entered an IP address and not random text or numbers.

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,15 +1,45 @@
-let cords = [42.84897787076336, -106.29737773388705];
-let map = L.map('map');
-updateView();
+let map = L.map("map").setView([0, 0], 2.5);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
-}).addTo(map); 
+//Set maps tile (the map appearance/colors)
+L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: "abcd",
+  maxZoom: 20,
+}).addTo(map);
 
-// Create marker and focus view on it
-function updateView(){
-   L.marker(cords).addTo(map);
-   map.setView(cords,18);
+// Create marker and focus view on it (Takes an array for the cords)
+function updateView(cords = [0, 0]) {
+  L.marker(cords).addTo(map);
+  map.setView(cords, 18);
 }
+
+//Fetch IP location
+let ip = "107.77.198.65"; //get this value from the text input
+let api_key = "at_MeEeSaIGcpXCKc2mFqNPgaqDrYQ2e";
+function getLocation() {
+  $.ajax({
+    url: "https://geo.ipify.org/api/v1",
+    data: { apiKey: api_key, ipAddress: ip },
+    success: function (data) {
+      alert("check console");
+      console.log(data);
+    },
+  });
+}
+
+//Entering Ip address
+let ipform = document.querySelector(".ip-form");
+let ipinput = document.querySelector(".ip-input");
+
+ipform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let ipformat =
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  if (ipinput.value.match(ipformat)) {
+    ipinput.value = "";
+    console.log("Sucess!!!");
+  } else {
+    alert("Invalid ip address!");
+  }
+});
