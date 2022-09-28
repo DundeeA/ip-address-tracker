491714f3ef3bd56a12084fa91aba6e69a186d8a4
dun <DundeeDevelops@gmail.com>
2022-09-27 22:05:32 -0500

    Changed map appearance

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,10 @@
 let map = L.map("map").setView([0, 0], 2.5);
 
 //Set maps tile (the map appearance/colors)
-L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: "abcd",
-  maxZoom: 20,
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
 }).addTo(map);
 
 // Create marker and focus view on it (Takes an array for the cords)
